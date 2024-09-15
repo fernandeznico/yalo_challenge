33cc5afef7bc6c2a351b0e31ee97cdf75109f66f
nico <nicofenane@gmail.com>
2024-09-15 00:58:07 -0500

    postgres.execute_query | Now use PostgresHook

--- dags/utils/postgres/__init__.py
+++ dags/utils/postgres/__init__.py
@@ -1,50 +1,9 @@
-import os
-
-import psycopg2
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 from utils.logging import log
 
 
-def execute_query(query: str, fetch_one: bool = True, commit: bool = False):
-    # Replace with your database credentials
-    host = "localhost" if os.environ.get("ENVIRONMENT") == "LOCAL" else "recalls_db"
-    database = "recalls_db"
-    user = "admin"
-    password = "admin"
-    port = 5433 if os.environ.get("ENVIRONMENT") == "LOCAL" else 5432
-
-    conn = None
-    cur = None
-
-    try:
-        # Connect to the database
-        conn = psycopg2.connect(
-            host=host,
-            database=database,
-            user=user,
-            password=password,
-            port=port,
-        )
-
-        # Create a cursor object to execute queries
-        cur = conn.cursor()
-
-        log.info(f"Executing the following query\n{query}")
-
-        # Execute the query
-        cur.execute(query)
-
-        if commit:
-            conn.commit()
-            if fetch_one:
-                return cur.rowcount
-
-        if fetch_one:
-            return cur.fetchone()
-
-    finally:
-        # Close the cursor and connection
-        if cur:
-            cur.close()
-        if conn:
-            conn.close()
+def execute_query(query: str, fetch_one: bool = True, commit: bool = False, conn_id: str = "recalls_db"):
+    postgres_hook = PostgresHook(postgres_conn_id=conn_id)
+    log.info(f"Executing the following query\n{query}")
+    return postgres_hook.run(sql=query, autocommit=commit, return_last=fetch_one)
